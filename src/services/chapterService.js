c9623f58c6945eff850761b9987990f4feb9589b
arnabbera <36722756+arnabbera@users.noreply.github.com>
2026-09-25 13:16:26 +0530

    Add IPC Chapter IXA election offences with BNS references

--- src/services/chapterService.js
+++ src/services/chapterService.js
@@ -45,6 +45,7 @@ const sourceControlledChapterIds = new Set([
   "criminal-law-i-transitioning-from-ipc-to-bns-unit-9",
   "criminal-law-i-transitioning-from-ipc-to-bns-unit-10",
   "criminal-law-i-transitioning-from-ipc-to-bns-unit-11",
+  "criminal-law-i-transitioning-from-ipc-to-bns-unit-12",
 ]);
 
 const reorderedCriminalLawIChapterIds = new Set([
